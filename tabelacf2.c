1438e8ee97a62188a3d210c3db1c26927c281f46
Norton Gaeta <ngaeta@gmail.com>
2018-10-15 18:00:58 -0300

    alteração para incluir constantes

--- tabelacf2.c
+++ tabelacf2.c
@@ -2,13 +2,17 @@
 
 #include "stdio.h"
 
+#define INICIO 0 /* valor inicial */
+#define FIM 300 /* valor final */
+#define INCR 20 /* incremento */
+
 int main()
 {
 
 	int fahr;
 
-	printf ("exemplo usando -for- \n");
-	for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+	printf ("\n exemplo usando -for- e com constantes \n");
+	for (fahr = INICIO; fahr <= FIM; fahr = fahr + INCR)
 		printf ("%4d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32) ) ;
 
 }
